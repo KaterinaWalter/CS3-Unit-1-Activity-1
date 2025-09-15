433a304218f081ff37499e8e87cf2c12da116003
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-09-15 16:39:46 +0000

    part a & b done

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+# DEFINE YOUR FUNCTIONS HERE!
+
+# PART A: Function with no args + no return
+def welcome_message():
+    print("Welcome to my virtual kitchen!")
+    print("Today, we're gonna make some pasta 🍝")
+
+# PART B: Function with args + no return
+def list_ingreds(main_ingredient, side_dish):
+    print(f"Main Ingredient: {main_ingredient}")
+    print(f"Side Dish: {side_dish}")
+
+# PART C: Function with args + return
+
+# PART D: Functions with optional arguments
+
+
 def main():
-    print("hello world")
+    # CALL YOUR FUNCTIONS in the main here
+    welcome_message()
+    list_ingreds("Cheese", "Broccoli")
+    list_ingreds("Beef", "Garlic Bread")
 
 
 if __name__ == "__main__":
     main()
+
